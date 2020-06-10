ea81935f5c4d7e1b244a3079ffc120a592163527
LiuHaixin <liuhaixin2016@outlook.com>
2020-06-10 16:36:19 +0800

    windjs

---
+++ DataVisualization/DataVisualization.Web/Controllers/WindJSController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DataVisualization.Web.Controllers
+{
+    public class WindJSController : Controller
+    {
+        // GET: WindJS
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Index2()
+        {
+            return View();
+        }
+
+        public ActionResult Layered()
+        {
+            return View();
+        }
+    }
+}
