b0efea71338807f06707acfe7eb66d3daea33e31
LiuHaixin <liuhaixin2016@outlook.com>
2020-06-10 14:04:28 +0800

    echarts

---
+++ DataVisualization/DataVisualization.Web/Controllers/EChartsController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DataVisualization.Web.Controllers
+{
+    public class EChartsController : Controller
+    {
+        // GET: ECharts
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Index2()
+        {
+            return View();
+        }
+
+        public ActionResult GlobalWindVisualization()
+        {
+            return View();
+        }
+
+        public ActionResult GlobalWindVisualization2()
+        {
+            return View();
+        }
+
+        public ActionResult Binning()
+        {
+            return View();
+        }
+
+        public ActionResult BMap()
+        {
+            return View();
+        }
+    }
+}
